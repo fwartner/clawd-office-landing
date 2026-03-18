02cd6b8839ac6437c0775d8c96ca581f4e8abaa5
Florian Wartner <florian@wartner.io>
2026-03-18 13:12:29 +0100

    feat: add PresenceStates and Rooms to page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { LangProvider } from "@/context/LangContext";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import WhatIs from "@/components/WhatIs";
+import PresenceStates from "@/components/PresenceStates";
+import Rooms from "@/components/Rooms";
 import Features from "@/components/Features";
 import TechStack from "@/components/TechStack";
 import Quickstart from "@/components/Quickstart";
@@ -16,6 +18,8 @@ export default function Home() {
       <Navbar />
       <Hero />
       <WhatIs />
+      <PresenceStates />
+      <Rooms />
       <Features />
       <TechStack />
       <Quickstart />
